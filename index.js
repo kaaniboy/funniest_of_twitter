3e2ab62058114967069aba133014bf1882a9f139
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-05-05 13:39:07 -0700

    Add function for retrieving video tweets

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 require('dotenv').config();
+const { getVideoTweets } = require('./twitter_utils');
+
+(async () => {
+    const tweets = await getVideoTweets('TheWorldOfFunny');
+    console.log(tweets);
+})();
